791054b5de7f31c6ad85a9eb173e827c4a4a9541
martinjhm271 <martinjhm271@hotmail.es>
2018-05-22 18:17:07 -0500

    menu,bar,fragment

--- app/src/main/java/bigheart/escuelaing/eci/edu/bigheart/ui/MainActivity.java
+++ app/src/main/java/bigheart/escuelaing/eci/edu/bigheart/ui/MainActivity.java
@@ -1,38 +1,147 @@
 package bigheart.escuelaing.eci.edu.bigheart.ui;
 
+
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 import bigheart.escuelaing.eci.edu.bigheart.R;
 
-public class MainActivity extends AppCompatActivity {
-    Button b1,b2;
-    Context c = this;
+public class MainActivity extends AppCompatActivity implements OnFragmentInteractionListener {
+
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
+    Context c =this;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        initNavigationDrawer();
+        Intent intent = new Intent(c, SingUpActivity.class);
+        startActivity(intent);
+    }
 
-        Button b1 = findViewById(R.id.r1);
-        Button b2 = findViewById(R.id.r2);
 
-        b1.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(c, RegistrationOrganizationActivity.class);
-                startActivity(intent);
+
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_events:
+                    showFragment(new TestFragment(), true);
+                    return true;
+                case R.id.navigation_myevents:
+                    showFragment(new Test2Fragment(), true);
+                    return true;
+                case R.id.navigation_myprofile:
+                    showFragment(new Test3Fragment(), true);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+
+
+
+    public void initNavigationDrawer() {
+
+        NavigationView navigationView = (NavigationView)findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                int id = menuItem.getItemId();
+
+                switch (id){
+                    case R.id.home:
+                        Toast.makeText(getApplicationContext(),"Home",Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawers();
+                        break;
+                    case R.id.settings:
+                        Toast.makeText(getApplicationContext(),"Settings",Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawers();
+                        break;
+                    case R.id.trash:
+                        Toast.makeText(getApplicationContext(),"Trash",Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawers();
+                        break;
+                    case R.id.logout:
+                        Toast.makeText(getApplicationContext(),"logout",Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawers();
+
+                }
+                return true;
             }
         });
-        b2.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(c, RegistrationVolunteerActivity.class);
-                startActivity(intent);
-            }
-        });
 
+        View header = navigationView.getHeaderView(0);
+        TextView tv_email = (TextView)header.findViewById(R.id.tv_email);
+        tv_email.setText("raj.amalw@learn2crack.com");
+
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer);
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.drawer_open,R.string.drawer_close){
+
+            @Override
+            public void onDrawerClosed(View v){
+                super.onDrawerClosed(v);
+            }
+
+            @Override
+            public void onDrawerOpened(View v) {
+                super.onDrawerOpened(v);
+            }
+        };
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+    }
+
+
+
+
+    public void showFragment(Fragment fragment, boolean addToBackStack)
+    {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+        String tag = fragment.getClass().getSimpleName();
+        if ( addToBackStack )
+        {
+            transaction.addToBackStack( tag );
+        }
+        transaction.replace( R.id.fragmentcontainer, fragment, tag );
+        transaction.commitAllowingStateLoss();
+    }
+
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
     }
 }
---
+++ app/src/main/java/bigheart/escuelaing/eci/edu/bigheart/ui/Test2Fragment.java
@@ -0,0 +1,50 @@
+package bigheart.escuelaing.eci.edu.bigheart.ui;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import bigheart.escuelaing.eci.edu.bigheart.R;
+
+
+public class Test2Fragment extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+
+    public Test2Fragment() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_test2, container, false);
+    }
+
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+}
